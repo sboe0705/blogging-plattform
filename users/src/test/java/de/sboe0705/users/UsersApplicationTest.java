db2b6588297329def60e502058515b5d17125403
Sebastian Böhm <mail@sboe0705.de>
2024-04-23 19:12:22 +0200

    added users module

---
+++ users/src/test/java/de/sboe0705/users/UsersApplicationTest.java
@@ -0,0 +1,14 @@
+package de.sboe0705.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTest {
+
+    @Test
+    void contextLoads() {
+        // an IllegalStateException is thrown if Spring Boot Application is not properly configured
+    }
+
+}
