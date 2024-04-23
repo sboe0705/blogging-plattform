d094ef4cd038dc1eff84c02b0a43bebca97be06b
Sebastian Böhm <mail@sboe0705.de>
2024-04-23 22:12:08 +0200

    added rest service for users persistence

--- users/src/main/java/de/sboe0705/users/persistence/UserRepository.java
+++ users/src/main/java/de/sboe0705/users/persistence/UserRepository.java
@@ -3,6 +3,7 @@ package de.sboe0705.users.persistence;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+// available as REST service under http://localhost:8080/api/users
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
